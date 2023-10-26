cd3a7a7f08a5fedcd6ffcc7d96faac19aa89818d
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-10-26 17:16:17 +1100

    setting up json web tokens

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,30 @@
+// check username, password in post(login) request
+// if exist create new JWT
+// send back to front end
+
+// setup authentication so only the request with JWT can access the dashboard
+
+const jwt = require("jsonwebtoken");
+
+const CustomAPIError = require("../errors/custom-error");
 const login = async (req, res) => {
-  res.send("Fake Login/Register/Signup Route");
+  const { username, password } = req.body;
+
+  //mongoose required validation
+  // Joi
+  //check in the controller
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
+  // just for demo, normally provided by DB;
+  const id = new Date().getDate();
+  // payload should be small as possible better experience for user
+  // In production use long, complex and unguessable string values for security
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  // console.log(username, password);
+  res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
